c0d41acaf6c5f83ae8bccdad1845f796315fcf4e
Maham Kanwal <kanwalmahsm@gmail.com>
2025-12-03 23:22:09 +0500

    feat: implement product API with RTK Query, add CRUD operations for products

---
+++ src/features/product/productApi2.js
@@ -0,0 +1,46 @@
+import { apiRequest, createTagOfData } from "../../utils/helperFunctions";
+import api, { tags } from "../baseApi";
+const { Products } = tags;
+ 
+const productApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getProducts: builder.query({
+      query: () => apiRequest("/products"),
+      providesTags: (result) => createTagOfData(result, Products),
+    }),
+ 
+    getProductById: builder.query({
+      query: (id) => apiRequest(`/products/${id}`),
+      providesTags: (result, error, id) => [{ type: Products, id }],
+    }),
+ 
+    addProduct: builder.mutation({
+      query: (newProduct) => apiRequest("/products", "POST", newProduct),
+      invalidatesTags: [{ type: Products, id: "LIST" }],
+    }),
+ 
+    updateProduct: builder.mutation({
+      query: ({ id, ...data }) => apiRequest(`/products/${id}`, "PUT", data),
+      invalidatesTags: (result, error, { id }) => [
+        { type: Products, id },
+      ],
+    }),
+ 
+    deleteProduct: builder.mutation({
+      query: (id) => apiRequest(`/products/${id}`, "DELETE"),
+      invalidatesTags: (result, error, id) => [
+        { type: Products, id },
+      ],
+    }),
+  }),
+});
+ 
+export const {
+  useGetProductsQuery,
+  useGetProductByIdQuery,
+  useAddProductMutation,
+  useUpdateProductMutation,
+  useDeleteProductMutation,
+} = productApi;
+ 
+export default productApi;
